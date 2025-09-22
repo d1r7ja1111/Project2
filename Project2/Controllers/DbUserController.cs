92a9c3329a5489013338bfe73b5fad240170fbd0
Darja <darja108@inbox.lv>
2025-09-22 10:49:40 +0300

    Server dala

---
+++ Project2/Controllers/DbUserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project2.Models;
+using Project2.Services.interfaces;
+
+
+namespace Project2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DbUserController(IUserRepository _repo) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10) //Pagination
+        {
+            var users = await _repo.GetAllUsers();
+            var paged = users.Skip((page - 1) * pageSize).Take(pageSize);
+            return Ok(paged);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await _repo.GetUserById(id);
+            return user is null ? NotFound() : Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserModel user)
+        {
+            var id = await _repo.Create(user);
+            return CreatedAtAction(nameof(Get), new { id }, user);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, UserModel user)
+        {
+            if (id != user.Id) return BadRequest("ID incorrect!");
+            await _repo.Update(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repo.Delete(id);
+            return NoContent();
+        }
+    }
+}
